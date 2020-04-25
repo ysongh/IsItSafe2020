2371b1f99b461edc53087fa31029570f42fb1f50
You Song Hou <yousonghou@yahoo.com>
2020-04-25 17:57:42 -0400

    install vue-router dependency and configure it

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div id="app">
     <Navbar />
-    <Landing />
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/layout/Navbar.vue';
-import Landing from './components/Landing.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar,
-    Landing
+    Navbar
   }
 }
 </script>
 
 <style>
-  *{
-    margin: 0;
-  }
 </style>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -5,18 +5,18 @@
         <div class="container">
           <div class="nav-wrapper">
             <a href="/" class="brand-logo">Logo</a>
-            <a href="#" data-target="mobile-nav" class="sidenav-trigger">
+            <router-link to="/" data-target="mobile-nav" class="sidenav-trigger">
                 <i class="material-icons">menu</i>
-            </a>
+            </router-link>
             <ul class="right hide-on-med-and-down">
                 <li>
-                    <a href="/">Home</a>
+                    <router-link to="/">Home</router-link>
                 </li>
                 <li>
-                    <a href="/customer">Customer</a>
+                    <router-link to="/about">Customer</router-link>
                 </li>
                 <li>
-                    <a href="/restaurants">Restaurants</a>
+                    <router-link to="/restaurants">Restaurants</router-link>
                 </li>
             </ul>
           </div>
@@ -26,13 +26,13 @@
 
     <ul class="sidenav" id="mobile-nav">
       <li>
-        <a href="/">Home</a>
+        <router-link to="/">Home</router-link>
       </li>
       <li>
-        <a href="/customer">Customer</a>
+        <router-link to="/about">Customer</router-link>
       </li>
       <li>
-        <a href="/restaurants">Restaurants</a>
+        <router-link to="/restaurants">Restaurants</router-link>
       </li>
     </ul>
   </div>
