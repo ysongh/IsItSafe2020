e54c3aabcc637e0ab78497fd0ecc8c501c927f7c
You Song Hou <yousonghou@yahoo.com>
2020-04-25 13:40:10 -0400

    set up vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Landing />
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
