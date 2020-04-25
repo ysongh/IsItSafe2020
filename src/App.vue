7bbe7c711173b18378a9ed331331da2f5362e1ad
You Song Hou <yousonghou@yahoo.com>
2020-04-25 16:37:08 -0400

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
+    <Navbar />
     <Landing />
   </div>
 </template>
 
 <script>
-import Landing from './components/Landing.vue'
+import Navbar from './components/layout/Navbar.vue';
+import Landing from './components/Landing.vue';
 
 export default {
   name: 'App',
   components: {
+    Navbar,
     Landing
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="navbar-fixed">
+      <nav class="light-blue darken-4">
+        <div class="container">
+          <div class="nav-wrapper">
+            <a href="/" class="brand-logo">Logo</a>
+            <a href="#" data-target="mobile-nav" class="sidenav-trigger">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/customer">Customer</a>
+                </li>
+                <li>
+                    <a href="/restaurants">Restaurants</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <ul class="sidenav" id="mobile-nav">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/customer">Customer</a>
+      </li>
+      <li>
+        <a href="/restaurants">Restaurants</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Navbar"
+  }
+</script>
