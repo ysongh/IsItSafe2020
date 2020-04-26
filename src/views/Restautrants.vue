c2457275cb740f0a0e291aeb282a455bdb442d4c
You Song Hou <yousonghou@yahoo.com>
2020-04-26 16:43:02 -0400

    add more restautrants content

--- src/views/Restautrants.vue
+++ src/views/Restautrants.vue
@@ -21,6 +21,66 @@
                     </div>
                 </div>
             </div>
+            <div class="col s12 m6">
+                <div class="card mt-2">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <router-link to="/restautrantDetail">
+                            <img class="activator" src="../assets/restautrant2.svg">
+                        </router-link>
+                    </div>
+                    <div class="card-content grey lighten-3">
+                        <span class="card-title activator grey-text text-darken-4">
+                            <h5 class="center-align">Restautrants 2</h5><i class="material-icons blue-text right">location_on</i>
+                        </span>
+                        <div class="card-info">
+                            <span class="card-p mr-5">Report</span><img src="../assets/icon2.svg" alt="Face">
+                        </div>
+                        <div class="card-info">
+                            <span class="card-p mr-2">Public opinion</span><img src="../assets/icon3.svg" alt="Face">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card mt-2">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <router-link to="/restautrantDetail">
+                            <img class="activator" src="../assets/restautrant3.svg">
+                        </router-link>
+                    </div>
+                    <div class="card-content grey lighten-3">
+                        <span class="card-title activator grey-text text-darken-4">
+                            <h5 class="center-align">Restautrants 3</h5><i class="material-icons blue-text right">location_on</i>
+                        </span>
+                        <div class="card-info">
+                            <span class="card-p mr-5">Report</span><img src="../assets/icon2.svg" alt="Face">
+                        </div>
+                        <div class="card-info">
+                            <span class="card-p mr-2">Public opinion</span><img src="../assets/icon3.svg" alt="Face">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card mt-2">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <router-link to="/restautrantDetail">
+                            <img class="activator" src="../assets/restautrant4.svg">
+                        </router-link>
+                    </div>
+                    <div class="card-content grey lighten-3">
+                        <span class="card-title activator grey-text text-darken-4">
+                            <h5 class="center-align">Restautrants 4</h5><i class="material-icons blue-text right">location_on</i>
+                        </span>
+                        <div class="card-info">
+                            <span class="card-p mr-5">Report</span><img src="../assets/icon2.svg" alt="Face">
+                        </div>
+                        <div class="card-info">
+                            <span class="card-p mr-2">Public opinion</span><img src="../assets/icon3.svg" alt="Face">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         
     </div>
