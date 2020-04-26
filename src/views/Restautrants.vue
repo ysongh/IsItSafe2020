78163868e9aa983b4868506d15635f1d347e133e
You Song Hou <yousonghou@yahoo.com>
2020-04-26 16:16:29 -0400

    change the layout of the restautrants page

--- src/views/Restautrants.vue
+++ src/views/Restautrants.vue
@@ -1,55 +1,28 @@
 <template>
     <div class="container">
-        <div class="card mt-2">
-            <div class="card-image waves-effect waves-block waves-light">
-                <router-link to="/restautrantDetail">
-                    <img class="activator" src="../assets/restautrant1.svg">
-                </router-link>
-            </div>
-            <div class="card-content light-blue darken-4">
-                <div>
-                    <p><i class="material-icons yellow-text">star</i><span class="white-text">120</span></p>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card mt-2">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <router-link to="/restautrantDetail">
+                            <img class="activator" src="../assets/restautrant1.svg">
+                        </router-link>
+                    </div>
+                    <div class="card-content grey lighten-2">
+                        <span class="card-title activator grey-text text-darken-4">
+                            <h5 class="center-align">Restautrants 1</h5><i class="material-icons blue-text right">location_on</i>
+                        </span>
+                        <div>
+                            <span>Report</span><img src="../assets/icon2.svg" alt="Face">
+                        </div>
+                        <div>
+                            <span>Public opinion</span><img src="../assets/icon3.svg" alt="Face">
+                        </div>
+                    </div>
                 </div>
-                <span class="card-title activator white-text center-align">Restautrants 1</span>
-                <i class="material-icons white-text">location_on</i>
             </div>
         </div>
-        <div class="card mt-2">
-            <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="../assets/restautrant4.svg">
-            </div>
-            <div class="card-content light-blue darken-4">
-                <div>
-                    <p><i class="material-icons yellow-text">star</i><span class="white-text">80</span></p>
-                </div>
-                <span class="card-title activator white-text center-align">Restautrants 2</span>
-                <i class="material-icons white-text">location_on</i>
-            </div>
-        </div>
-        <div class="card mt-2">
-            <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="../assets/restautrant3.svg">
-            </div>
-            <div class="card-content light-blue darken-4">
-                <div>
-                    <p><i class="material-icons yellow-text">star</i><span class="white-text">60</span></p>
-                </div>
-                <span class="card-title activator white-text center-align">Restautrants 3</span>
-                <i class="material-icons white-text">location_on</i>
-            </div>
-        </div>
-        <div class="card mt-2">
-            <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="../assets/restautrant2.svg">
-            </div>
-            <div class="card-content light-blue darken-4">
-                <div>
-                    <p><i class="material-icons yellow-text">star</i><span class="white-text">45</span></p>
-                </div>
-                <span class="card-title activator white-text center-align">Restautrants 4</span>
-                <i class="material-icons white-text">location_on</i>
-            </div>
-        </div>
+        
     </div>
   
 </template>
@@ -75,9 +48,6 @@ export default {
 
     .card-content{
         padding: 3px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
     }
 
     .card-title{
