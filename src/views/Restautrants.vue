a8d78b866f7cd7cfe03e7667c623fb025a153bf5
You Song Hou <yousonghou@yahoo.com>
2020-04-26 16:31:38 -0400

    add more styles on the card

--- src/views/Restautrants.vue
+++ src/views/Restautrants.vue
@@ -8,15 +8,15 @@
                             <img class="activator" src="../assets/restautrant1.svg">
                         </router-link>
                     </div>
-                    <div class="card-content grey lighten-2">
+                    <div class="card-content grey lighten-3">
                         <span class="card-title activator grey-text text-darken-4">
                             <h5 class="center-align">Restautrants 1</h5><i class="material-icons blue-text right">location_on</i>
                         </span>
-                        <div>
-                            <span>Report</span><img src="../assets/icon2.svg" alt="Face">
+                        <div class="card-info">
+                            <span class="card-p mr-5">Report</span><img src="../assets/icon2.svg" alt="Face">
                         </div>
-                        <div>
-                            <span>Public opinion</span><img src="../assets/icon3.svg" alt="Face">
+                        <div class="card-info">
+                            <span class="card-p mr-2">Public opinion</span><img src="../assets/icon3.svg" alt="Face">
                         </div>
                     </div>
                 </div>
@@ -39,7 +39,16 @@ export default {
     }
 
     i{
-        font-size: 1.8rem;
+        font-size: 2.5rem;
+    }
+
+    img{
+        width: 1.5rem;
+    }
+
+    h5{
+        margin-top: .5rem;
+        margin-bottom: 1rem;
     }
 
     .card{
@@ -47,15 +56,32 @@ export default {
     }
 
     .card-content{
-        padding: 3px;
+        padding: 3px 7px 1.6rem 7px;
     }
 
     .card-title{
         font-size: 1.5rem;
         font-weight: 100 !important;
     }
+
+    .card-info{
+        display: flex;
+        padding: 3px 0;
+    }
+
+    .card-p{
+        font-size: 16px;
+        font-weight: 100;
+    }
     
     .mt-2{
         margin-top: 2rem;
+    }
+    .mr-2{
+        margin-right: 38px;
+    }
+
+    .mr-5{
+        margin-right: 90px;
     }
 </style>
